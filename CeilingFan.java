5054a1d79d2bc2cdcca23c670cc101f1f59a65e0
Kanak Prajapati <kn436428@dal.ca>
2022-08-08 14:46:22 -0300

    Methods for CeilingFan

--- CeilingFan.java
+++ CeilingFan.java
@@ -1,37 +1,54 @@
 public class CeilingFan {
-    private int MAX_SPEED;
+
+    private int MAX_SPEED = 3;      // Constant for max speed
     private int presentSpeed;
     private boolean reverse;
 
     public CeilingFan() {
-   
+        this.presentSpeed = 0;       // Default off
+        this.reverse = false;
     }
 
     public int getMAX_SPEED() {
-  
+        return MAX_SPEED;
     }
 
     public int getPresentSpeed() {
+        return presentSpeed;
     }
-       
 
     public void setPresentSpeed(int presentSpeed) {
-       
+        this.presentSpeed = presentSpeed;
     }
 
     public boolean isReverse() {
-      
+        return reverse;
     }
 
     public void setReverse(boolean reverse) {
-      
+        this.reverse = reverse;
     }
 
     public void changeSpeed() {
-     
+        if (isReverse()) {
+            if (getPresentSpeed() == 0) {
+                setPresentSpeed(getMAX_SPEED());
+            }
+            else {
+                setPresentSpeed(getPresentSpeed() - 1);
+            }
+        }
+        else {
+            if (getPresentSpeed() == getMAX_SPEED()) {
+                setPresentSpeed(0);
+            }
+            else {
+                setPresentSpeed(getPresentSpeed() + 1);
+            }
+        }
     }
 
     public void changeSetting () {
-        
+        setReverse(!isReverse());
     }
 }
