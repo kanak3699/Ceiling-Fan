9f7bb7f4b3216f9d401ffbcadd8520e2a80e7296
Kanak Prajapati <kn436428@dal.ca>
2022-08-08 13:51:42 -0300

    CeilingFan Main File

---
+++ CeilingFan.java
@@ -0,0 +1,37 @@
+public class CeilingFan {
+    private int MAX_SPEED;
+    private int presentSpeed;
+    private boolean reverse;
+
+    public CeilingFan() {
+   
+    }
+
+    public int getMAX_SPEED() {
+  
+    }
+
+    public int getPresentSpeed() {
+    }
+       
+
+    public void setPresentSpeed(int presentSpeed) {
+       
+    }
+
+    public boolean isReverse() {
+      
+    }
+
+    public void setReverse(boolean reverse) {
+      
+    }
+
+    public void changeSpeed() {
+     
+    }
+
+    public void changeSetting () {
+        
+    }
+}
